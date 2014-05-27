49406acd46dab95804800e6941e9c7b5a139ce58
Dani Postigo <dani@firstperson.is>
2014-05-27 06:28:59 -0400

    updates

--- DPKit-Utils/ios/NSObject+InterfaceUtils.h
+++ DPKit-Utils/ios/NSObject+InterfaceUtils.h
@@ -6,6 +6,12 @@
 
 @interface NSObject (InterfaceUtils)
 
+- (UIInterfaceOrientation) statusOrientation;
+- (NSString *) stringForInterfaceOrientation: (UIInterfaceOrientation) orientation;
+- (UIDeviceOrientation) deviceOrientation;
+- (NSString *) deviceOrientationAsString;
+- (NSString *) stringForDeviceOrientation: (UIDeviceOrientation) orientation;
+- (NSString *) statusOrientationAsString;
 - (BOOL) isLandscapeLeft;
 - (BOOL) isLandscapeRight;
 - (BOOL) isLandscape;
