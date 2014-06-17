72f63565b6439a91ce6b714ea5d942caa49ec0ad
Dani Postigo <dani.postigo@gmail.com>
2014-06-17 10:17:13 -0400

    updates

--- DPKit-Utils/ios/UIViewController+DPKit.m
+++ DPKit-Utils/ios/UIViewController+DPKit.m
@@ -2,34 +2,34 @@
 // Created by Dani Postigo on 6/6/14.
 //
 
-#import <DPKit-UIView/UIView+DPConstraints.h>
 #import "UIViewController+DPKit.h"
 
 @implementation UIViewController (DPKit)
 
-- (void (^)()) constraintAnimationBlock {
+- (void (^)())constraintAnimationBlock {
     return ^{
         [self.view layoutIfNeeded];
     };
 }
 
 
-- (void) registerKeyboardWillShow: (SEL) selector {
-    [[NSNotificationCenter defaultCenter] addObserver: self selector: selector name: UIKeyboardWillShowNotification object: nil];
+- (void)registerKeyboardWillShow:(SEL)selector {
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:selector name:UIKeyboardWillShowNotification object:nil];
 }
 
 
-- (void) embedFullscreenController: (UIViewController *) controller {
+- (void)embedFullscreenController:(UIViewController *)controller {
     UIView *view = controller.view;
-    [self addChildViewController: controller];
-    [self.view addSubview: view];
+    [self addChildViewController:controller];
+    [self.view addSubview:view];
 
     view.frame = self.view.bounds;
     view.translatesAutoresizingMaskIntoConstraints = NO;
-    [view updateSuperLeadingConstraint: 0];
-    [view updateSuperTrailingConstraint: 0];
-    [self.view addConstraint: [NSLayoutConstraint constraintWithItem: view attribute: NSLayoutAttributeTop relatedBy: NSLayoutRelationEqual toItem: self.topLayoutGuide attribute: NSLayoutAttributeTop multiplier: 1.0 constant: 0.0]];
-    [self.view addConstraint: [NSLayoutConstraint constraintWithItem: view attribute: NSLayoutAttributeBottom relatedBy: NSLayoutRelationEqual toItem: self.bottomLayoutGuide attribute: NSLayoutAttributeTop multiplier: 1.0 constant: 0.0]];
+
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeading multiplier:1.0 constant:0.0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:0.0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.topLayoutGuide attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.bottomLayoutGuide attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.0]];
 
 }
 
