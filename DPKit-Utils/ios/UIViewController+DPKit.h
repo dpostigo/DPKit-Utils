737e740ff22c688c0d273935c3922e024a42e774
Dani Postigo <danielle.postigo@aexp.com>
2014-10-24 16:53:54 -0400

    Household offers doesn't have data. PointsActivity giving me grief.

--- DPKit-Utils/ios/UIViewController+DPKit.h
+++ DPKit-Utils/ios/UIViewController+DPKit.h
@@ -8,5 +8,17 @@
 
 - (void (^)()) constraintAnimationBlock;
 - (void) registerKeyboardWillShow: (SEL) selector;
+- (void) registerKeyboardWillHide: (SEL) selector;
+- (id) viewControllerFromStoryboard;
+- (id) viewControllerFromStoryboard: (NSString *) storyboardName;
+- (UITapGestureRecognizer *) addTapBehindRecognizer: (SEL) selector;
+- (UITapGestureRecognizer *) addTapBehindRecognizerWithBlock: (void (^)()) block;
+- (BOOL) recognizerDidTapBehind: (UITapGestureRecognizer *) sender;
+- (UITapGestureRecognizer *) addTapBehindRecognizer;
 - (void) embedFullscreenController: (UIViewController *) controller;
+- (void) embedFullscreenController: (UIViewController *) controller withInsets: (UIEdgeInsets) insets;
+- (void) embedFullscreenView: (UIView *) view;
+- (void) embedFullscreenView: (UIView *) view withInsets: (UIEdgeInsets) insets;
+- (void) unembedController: (UIViewController *) controller;
+- (void) embedController: (UIViewController *) controller inView: (UIView *) parentView;
 @end
